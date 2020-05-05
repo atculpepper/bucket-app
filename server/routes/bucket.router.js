e07d7fef2fc527b7805f29ed3c314fa57906f304
Anne Culpepper <anne.culpepper@gmail.com>
2020-05-05 14:46:39 -0500

    wrote a GET route on bucket.router.js that GETS user experiences based on user id

--- server/routes/bucket.router.js
+++ server/routes/bucket.router.js
@@ -5,7 +5,23 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get("/", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const userID = req.params.id;
+  const queryText = `SELECT "user_experiences".user_id, "experiences".description
+    FROM "user_experiences" JOIN "experiences" ON "user_experiences".experience_id = "experiences".id
+    WHERE "user_id" = $1;`;
+  pool
+    .query(queryText, [userID])
+    .then((responseDB) => {
+      const dbRows = responseDB.rows;
+      console.table(dbRows);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("error:", err);
+      res.sendStatus(500);
+    });
+});
 
 /**
  * POST route template
