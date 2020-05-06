448c4d34bf7c64f96c94c0b9791481da45137f22
Anne Culpepper <anne.culpepper@gmail.com>
2020-05-06 15:07:44 -0500

    updated styling in nav and across theme to experiment with design

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -32,7 +32,7 @@ const Nav = (props) => {
         {props.store.user.id && (
           <>
             <Link className="nav-link" to="/info">
-              Info Page
+              User Profile
             </Link>
             <LogOutButton className="nav-link" />
             <Link className="nav-link" to="/explore">
