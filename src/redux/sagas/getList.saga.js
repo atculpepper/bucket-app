83fd1a7fc0ac14c5d0caee2122dd01171c03abe8
Anne Culpepper <anne.culpepper@gmail.com>
2020-05-06 16:33:47 -0500

    connected the getList saga to the root saga and changed the state in the reducer from an array to an object. Stopped mapping the object and started pulling out data from the getList reducer to display list items associated with user id on the DOM on UserPage

--- src/redux/sagas/getList.saga.js
+++ src/redux/sagas/getList.saga.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 
-function* getList() {
+function* getList(action) {
   try {
-    const itemID = payload;
-    const response = yield axios.get(`/api/user/${itemID}`);
+    const userID = action.payload;
+    const response = yield axios.get(`/api/user/${userID}`);
     yield put({
       type: "SET_LIST_ITEMS",
+      payload: response.data[0],
     });
   } catch (err) {
     console.warn(err);
@@ -15,6 +16,7 @@ function* getList() {
 
 function* getListSaga() {
   yield takeLatest("GET_LIST_ITEMS", getList);
+  console.log("getListSaga fired off");
 }
 
 export default getListSaga;
