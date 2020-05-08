a357d77cab063d4de5aa95d1df02103ae171eab6
Anne Culpepper <anne.culpepper@gmail.com>
2020-05-08 11:24:39 -0500

    fixed POST route. Next I need to trigger the GET so that page can re-render

--- server/routes/bucket.router.js
+++ server/routes/bucket.router.js
@@ -32,31 +32,37 @@ router.get("/:id", rejectUnauthenticated, (req, res) => {
 /**
  * POST route template to add list item (link with user id) STILL NEEDS WORK
  */
-router.post("/:id", rejectUnauthenticated, (req, res) => {
+router.post("/:id", (req, res) => {
   let newItem = req.body;
   let userID = req.params.id;
 
   console.log(`Adding item`, newItem.bucketItem);
   let queryText = `INSERT INTO "experiences" ("description") VALUES ($1) RETURNING id;`;
-  //use await to save experience
-  // const savedExperience = await;
-  pool.query(queryText, [newItem.bucketItem]).then((response) => {
-    const experienceID = response.rows[0].id;
-    console.log(userID);
-    pool
-      .query(
-        `INSERT INTO "user_photos_experiences" ("user_id", "experience_id") VALUES ($1, $2);`,
-        [(userID, experienceID)]
-      )
-      .then((responseDB) => {
-        const dbRows = responseDB.rows;
-        console.table(dbRows);
-        res.send(dbRows);
-      })
-      .catch((err) => {
-        res.sendStatus(500);
-      });
-  });
+
+  pool
+    .query(queryText, [newItem.bucketItem])
+    .then((responseDB) => {
+      const experienceID = responseDB.rows[0].id;
+
+      pool
+        .query(
+          `INSERT INTO "user_photos_experiences" ("user_id", "experience_id") VALUES ($1, $2);`,
+          [userID, experienceID]
+        )
+        .then((responseDB) => {
+          const dbRows = responseDB.rows;
+          console.table(dbRows);
+          res.send(dbRows);
+        })
+        .catch((err) => {
+          console.log("err inserting to experiences", err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      console.log("err inserting to user_photos_experiences", err);
+      res.sendStatus(500);
+    });
 });
 
 //PUT route to update single list item
